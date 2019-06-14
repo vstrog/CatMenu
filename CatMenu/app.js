b4539c6119e53828f001cb93323a5377b40dc753
vstrog <39245801+vstrog@users.noreply.github.com>
2019-06-14 19:38:51 +0300

    Add files via upload

---
+++ CatMenu/app.js
@@ -0,0 +1,10 @@
+document.querySelector('.triple-menu').addEventListener('click', () => {
+    document.querySelector('.nav-wrapper').classList.toggle('change');
+});
+
+
+//Makes button summon menu
+
+/* document.querySelector('.banner-btn').addEventListener('click', () => {
+     document.querySelector('.nav-wrapper').classList.toggle('change');
+ }); */
